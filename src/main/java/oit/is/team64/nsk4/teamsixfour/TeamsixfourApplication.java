c91b454dec1fa5db38a6360d767299b568746334
NakamuraKenshiro <e1b23061@oit.ac.jp>
2025-10-28 14:09:56 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team64/nsk4/teamsixfour/TeamsixfourApplication.java
@@ -0,0 +1,13 @@
+package oit.is.team64.nsk4.teamsixfour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsixfourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsixfourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team64/nsk4/teamsixfour/TeamsixfourApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team64.nsk4.teamsixfour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsixfourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
